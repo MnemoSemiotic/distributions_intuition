88d530345370c01dbdfaafc93f9d7e86d2ac4ac1
Andrew Roberts <clownfragment@gmail.com>
2020-05-14 19:50:57 -0600

    ee

---
+++ binomial_distr.py
@@ -0,0 +1,13 @@
+
+def dec_to_bin(dec, n_bits=8):
+    bin_list = []
+    x = dec
+
+    for _ in range(n_bits):
+        bit = x % 2
+        bin_list.append(bit)
+        x //= 2
+
+    return bin_list[::-1]
+
+print(dec_to_bin(255))
