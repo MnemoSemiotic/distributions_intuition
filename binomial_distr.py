add91b2dd5cfcd560bf00595ba61ee065e7b7ea2
Andrew Roberts <clownfragment@gmail.com>
2020-05-14 21:28:01 -0600

    finish

--- binomial_distr.py
+++ binomial_distr.py
@@ -1,4 +1,18 @@
 
+
+def gen_8_bit_bin_with_fors():
+    bin_lst = []
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                for l in range(2):
+                    for m in range(2):
+                        for n in range(2):
+                            for o in range(2):
+                                for p in range(2):
+                                    bin_lst.append([i,j,k,l,m,n,o,p])
+    return bin_lst
+
 def dec_to_bin(dec, n_bits=8):
     bin_list = []
     x = dec
