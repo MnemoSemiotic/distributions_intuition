35afb553190211b55b985fe1eb414cf497f7befc
Andrew Roberts <clownfragment@gmail.com>
2020-05-14 20:39:10 -0600

    ii

--- binomial_sampling.py
+++ binomial_sampling.py
@@ -9,3 +9,22 @@ def generate_n_bits(n=12):
         lst.append(get_bit())
     return lst
 
+def binary_sampling_dict(n_bits, num_samples=500):
+    d = {}
+
+    for _ in range(num_samples):
+        binary = generate_n_bits(n=n_bits)
+        sum_bits = sum(binary)
+
+        if sum_bits not in d:
+            d[sum_bits] = 0
+        d[sum_bits] += 1
+
+    return d
+
+d = binary_sampling_dict(num_samples=500)
+
+for k, v in d.items():
+    print(f'{k}: {v}')
+        
+
