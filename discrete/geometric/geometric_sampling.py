3d7d9ae1acc91b01dcb3b3916a8cc32881934870
Andrew Roberts <clownfragment@gmail.com>
2020-05-21 20:31:46 -0600

    add geometric func

--- discrete/geometric/geometric_sampling.py
+++ discrete/geometric/geometric_sampling.py
@@ -10,10 +10,24 @@ def get_bernoulli(p=0.5):
     return 0
 
 
-for _ in range(2000):
-    flip = get_bernoulli(p)
-    if flip == 1:
-        print('SUCCESS!')
-        break
-    print(f'trial: {flip}')
+# for _ in range(2000):
+#     flip = get_bernoulli(p)
+#     if flip == 1:
+#         print('SUCCESS!')
+#         break
+#     print(f'trial: {flip}')
+
+def geometric(p=0.5):
+    lst = []
+
+    for _ in range(10000000000):
+        trial = get_bernoulli(p)
+        lst.append(trial)
+
+        if trial == 1:
+            break
+
+    return len(lst)-1
+
+# print(geometric(p=0.2))
 
