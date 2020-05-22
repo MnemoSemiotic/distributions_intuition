05a118ef3a26e6417d65ca400017f7f682ac286a
Andrew Roberts <clownfragment@gmail.com>
2020-05-21 19:29:23 -0600

    add geom func

--- discrete/geometric/geometric_using_pmf.py
+++ discrete/geometric/geometric_using_pmf.py
@@ -2,4 +2,16 @@
 def geometric_pmf(p, k):
     return ((1-p)**k)*p
 
-print(geometric_pmf(0.32, 2))
+
+def geometric_cdf_closed(p, k):
+    return 1 - (1-p)**(k+1)
+
+def geometric_cdf(p, k):
+    proba_ = 0
+
+    for r in range(k):
+        proba_ += geometric_pmf(p, r)
+    return proba_
+
+# print(geometric_pmf(0.32, 2))
+
